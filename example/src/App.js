7ea2acd42ef8e20ad4322ca3fe2c797c0d4060ba
Janardhan Bobba <janardhan.bobba@dreamorbit.com>
2021-07-28 06:43:08 +0530

    updated

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-common-componets'
+import 'react-common-componets/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
